40927034afea1ad28ebcc6d0797251b67b221321
siri-priyanka <120650732+siri-priyanka@users.noreply.github.com>
2025-06-25 22:23:07 +0530

    Update SpringDataJpaExApplicationTests.java

--- src/test/java/com/raghu/spring_data_jpa_ex/SpringDataJpaExApplicationTests.java
+++ src/test/java/com/raghu/spring_data_jpa_ex/SpringDataJpaExApplicationTests.java
@@ -3,11 +3,10 @@ package com.raghu.spring_data_jpa_ex;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SpringDataJpaExApplicationTests {
+@SpringBootTest(classes = SpringDataJpaExApplication.class)  // <-- Add this line
+public class SpringDataJpaExApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
